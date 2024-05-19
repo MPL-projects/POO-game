8a9b749429953afadacb8f76eb868de0d0c847cb
Louis LE LAY <le.lay.louis@gmail.com>
2024-05-19 13:21:13 +0200

    creation of the classes for an health bar

---
+++ include/health_bar.hpp
@@ -0,0 +1,37 @@
+#ifndef HEALTH_BAR_HPP
+#define HEALTH_BAR_HPP
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_image.h>
+#include <string>
+#include <stdexcept>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class HealthBar {
+public:
+    HealthBar(int x, int y, int width, int height);
+    ~HealthBar();
+
+    int get_x() const {return m_rect.x;};
+    int get_y() const {return m_rect.y;};
+    int get_w() const {return m_rect.w;};
+    int get_h() const {return m_rect.h;};
+
+	void punched();
+    
+    void render();
+
+private:
+    SDL_Rect m_rect;
+
+    vector<SDL_Texture*> m_textures;
+    SDL_Renderer* m_renderer;
+    int m_currentTextureIndex;
+    SDL_Texture* m_textTexture;
+};
+
+#endif
---
+++ src/health_bar.cpp
@@ -0,0 +1,41 @@
+#include "../include/health_bar.hpp"
+#include "../include/game.hpp"
+#include "../include/utilities.hpp"
+
+HealthBar::HealthBar(int x, int y, int width, int height): 
+               m_rect({x, y, width, height}), 
+               m_renderer(Game::appWindow->renderer),
+               m_currentTextureIndex(0)
+{
+	std::vector<std::string> imagePaths = {	"assets/images/backgrounds_elements/menu/buttons/left.png", 
+											"assets/images/backgrounds_elements/menu/buttons/left.png", 
+											"assets/images/backgrounds_elements/menu/buttons/left.png"};
+
+    for (const auto &imagePath : imagePaths)
+    {
+        SDL_Surface *surface = load_png(imagePath.c_str());
+        SDL_Texture *texture = SDL_CreateTextureFromSurface(m_renderer, surface);
+        SDL_FreeSurface(surface);
+        m_textures.push_back(texture);
+    }
+}
+
+HealthBar::~HealthBar()
+{
+    for (auto& texture : m_textures)
+    {
+        SDL_DestroyTexture(texture);
+    }
+    m_textures.clear(); // Clear the vector after destroying the textures
+}
+
+void HealthBar::render()
+{
+    SDL_RenderCopy(m_renderer, m_textures[m_currentTextureIndex], NULL, &m_rect);
+}
+
+
+void HealthBar::punched()
+{
+    
+}
